d1923cae6ed41718eaa0b30804714e736f58d378
Elijah <sargeantfishcoc@gmail.com>
2020-01-02 12:37:58 -0500

    On-the-fly field oriented switching; updated to 2020 PID

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,12 +2,12 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
 import edu.wpi.first.networktables.*;
@@ -22,20 +22,10 @@ public class Robot extends TimedRobot {
 	Timer timer = new Timer();
 
 	AHRS navX;
-	/*
-	 * public AnalogInput backRightEncoder = new AnalogInput(0); public AnalogInput
-	 * backLeftEncoder = new AnalogInput(1); public AnalogInput frontRightEncoder =
-	 * new AnalogInput(2); public AnalogInput frontLeftEncoder = new AnalogInput(3);
-	 */
+
 	public XboxController Controller = new XboxController(0);
-
-	// frontleft --> frontright
-	// frontright --> backright
-	// backright --> backleft
-	// backleft --> frontleft
-
-	private WheelDrive backRight = new WheelDrive(3, 4, 0, 0.78715, 0.0004, 0.0);// actual port ID's of angle and speed
-																					// motors
+	
+	private WheelDrive backRight = new WheelDrive(3, 4, 0, 0.78715, 0.0004, 0.0);// actual port ID's of angle and speed motors																			// motors
 	private WheelDrive backLeft = new WheelDrive(1, 2, 1, 0.79715, 0.0006, 0.0);// (In that order)
 	private WheelDrive frontRight = new WheelDrive(5, 6, 2, 0.68715, 0.0005, 0.0);
 	private WheelDrive frontLeft = new WheelDrive(7, 8, 3, 0.88715, 0.0003, 0.0);
@@ -77,10 +67,10 @@ public class Robot extends TimedRobot {
 
 		while (timer.get() < 3) {
 
-			swerveDrive.drive(-1, 0, 0, navX.getAngle() - 90);
+			swerveDrive.drive(-1, 0, 0, navX.getAngle() - 90, false);
 		}
 		
-		swerveDrive.drive(0, 0, 0, navX.getAngle() - 90);
+		swerveDrive.drive(0, 0, 0, navX.getAngle() - 90, false);
 	}
 
 	
@@ -99,25 +89,13 @@ public class Robot extends TimedRobot {
 			swerveDrive.zeroEncoders();			
 		}
 		
-<<<<<<< Updated upstream
-		swerveDrive.drive (Controller.getRawAxis(1), Controller.getRawAxis(0), Controller.getRawAxis(4), navX.getAngle() - 90);//navX.getAngle() - 90); //get them inputs
+		swerveDrive.drive(Controller.getRawAxis(1), Controller.getRawAxis(0), Controller.getRawAxis(4), navX.getAngle() - 90, Controller.getStickButton(Hand.kLeft));// navX.getAngle() - 90); //get them inputs
 
 		if (Controller.getAButton()) {
 
 			navX.zeroYaw();
 			System.out.println("Zeroed.");
 		}
-
-		//System.out.println("Controller in: " + Controller.getRawAxis(1));
-=======
-		swerveDrive.drive (Controller.getRawAxis(1), Controller.getRawAxis(0), Controller.getRawAxis(4), ahrs.getAngle()- 90); //get them inputs
->>>>>>> Stashed changes
-		/*
-		System.out.print("Back Right Encoder: " + backRightEncoder.getVoltage());
-		System.out.print("Back Left Encoder: " + backLeftEncoder.getVoltage());
-		System.out.print("Front Right Encoder: " + frontRightEncoder.getVoltage());
-		System.out.println("Front Left Encoder: " + frontLeftEncoder.getVoltage());
-		*/
 	}
 
 	
--- src/main/java/frc/robot/SwerveDrive/SwerveDrive.java
+++ src/main/java/frc/robot/SwerveDrive/SwerveDrive.java
@@ -18,13 +18,14 @@ public class SwerveDrive {
 	public final double CONTROLLER_DEADBAND = .05;
 	
 	/**
-	 * 
+	 * Use for human input
 	 * @param x1 Forward input
 	 * @param y1 Strafe input
 	 * @param rotation Azimuth input
 	 * @param theta Gyro Yaw input
+	 * @param robotCentric 
 	 */
-	public void drive (double x1, double y1, double rotation, double theta) {
+	public void drive (double x1, double y1, double rotation, double theta, boolean robotCentric) {
 
 		if (CONTROLLER_DEADBAND * -1 < x1 && x1 < CONTROLLER_DEADBAND && CONTROLLER_DEADBAND * -1 < y1 && 
 			y1 < CONTROLLER_DEADBAND && CONTROLLER_DEADBAND * -1 < rotation && rotation < CONTROLLER_DEADBAND){
@@ -34,15 +35,26 @@ public class SwerveDrive {
 
 		else{ deadband = false; }
 
+		double temp;
+		double strafe;
+		double forward;
+
 		double r = Math.hypot(L, W);
 	
-		
-		double temp = y1 * Math.cos(Math.toRadians(theta)) + x1 * Math.sin(Math.toRadians(theta)); //allows for field centric control
-		
-		double strafe = -y1 * Math.sin(Math.toRadians(theta)) + x1 * Math.cos(Math.toRadians(theta));
+		if (robotCentric) {
 
-		double forward = temp;
+			forward = x1;
+			strafe  = y1;
+		}
+
+		else {
+
+			temp = y1 * Math.cos(Math.toRadians(theta)) + x1 * Math.sin(Math.toRadians(theta)); //allows for field centric control
 		
+			strafe = -y1 * Math.sin(Math.toRadians(theta)) + x1 * Math.cos(Math.toRadians(theta));
+
+			forward = temp;
+		}
 		
 		double a = strafe - rotation  * (L / r); //placeholder vector values
 		
@@ -82,7 +94,7 @@ public class SwerveDrive {
 
 		if (backLeftAngle > 5) {backLeftAngle = backLeftAngle - 5;}
 		if (backLeftAngle < 0) {backLeftAngle = 5 - backLeftAngle;}
-		//									-1 to 1		   -2.5 to 2.5   0 to 5  whatever the fuck it wants to be
+		//									-1 to 1		   -2.5 to 2.5   0 to 5  
 		double frontRightAngle	= (((Math.atan2(b, c) / Math.PI) * 2.5) + 2.5) + ANGLE_OFFSET3;
 		
 		if (frontRightAngle > 5) {frontRightAngle = frontRightAngle - 5;}
@@ -118,21 +130,6 @@ public class SwerveDrive {
 	}
 
 
-	public void zeroEncoders(){
-
-		ANGLE_OFFSET1 = backRight.encoderValue;
-
-		ANGLE_OFFSET2 = backLeft.encoderValue;
-
-		ANGLE_OFFSET3 = frontRight.encoderValue;
-
-		ANGLE_OFFSET4 = frontLeft.encoderValue;
-
-		System.out.println(ANGLE_OFFSET1 + " " + ANGLE_OFFSET2 + " " + ANGLE_OFFSET3 + " " + ANGLE_OFFSET4);
-		System.out.println("Encoder Angles Set!");
-	}
-
-		
 		private WheelDrive backRight;
 		
 		private WheelDrive backLeft;
@@ -140,6 +137,100 @@ public class SwerveDrive {
 		private WheelDrive frontRight;
 		
 		private WheelDrive frontLeft;
+
+		public void zeroEncoders(){
+
+			ANGLE_OFFSET1 = backRight.getAzimuth();
+	
+			ANGLE_OFFSET2 = backLeft.getAzimuth();
+	
+			ANGLE_OFFSET3 = frontRight.getAzimuth();
+	
+			ANGLE_OFFSET4 = frontLeft.getAzimuth();
+	
+			System.out.println(ANGLE_OFFSET1 + " " + ANGLE_OFFSET2 + " " + ANGLE_OFFSET3 + " " + ANGLE_OFFSET4);
+			System.out.println("Encoder Angles Set!");
+		}
+
+		/**
+		 * Use for interfacing swerve modules with vision data
+		 * @param horError  Horizontal error 
+		 * @param vertError Vertical error
+		 * @param rotError 	Rotational error
+		 */
+		public void visionInterface(double horError, double vertError, double rotError) {
+
+			double r = Math.hypot(L, W);
+		
+			double a = horError  -   rotError * (L / r); //placeholder vector values
+			
+			double b = horError  +   rotError * (L / r);
+		
+			double c = vertError -  rotError * (W / r);
+		
+			double d = vertError +  rotError * (W / r);
+		
+		
+			double autoBackRightSpeed = 0; //calculating speed
+				
+			double autoBackLeftSpeed = 0;
+				
+			double autoFrontRightSpeed = 0;
+				
+			double autoFrontLeftSpeed = 0;
+		
+		
+			//Output is 0 to 1
+			autoBackRightSpeed = Math.hypot(a, c);
+			
+			autoBackLeftSpeed = Math.hypot(a, d);
+
+			autoFrontRightSpeed = Math.hypot(b, c);
+					
+			autoFrontLeftSpeed = Math.hypot(b, d);
+		
+
+			//Output is 0 to 360 degrees
+			double autoBackRightAngle 	= (((Math.atan2(a, c) / Math.PI) * 2.5) + 2.5) + ANGLE_OFFSET1; 
+
+			if (autoBackRightAngle > 5) {autoBackRightAngle = autoBackRightAngle - 5;}
+			if (autoBackRightAngle < 0) {autoBackRightAngle = 5 - autoBackRightAngle;}
+		
+			double autoBackLeftAngle 	= (((Math.atan2(a, d) / Math.PI) * 2.5) + 2.5) + ANGLE_OFFSET2;
+
+			if (autoBackLeftAngle > 5) {autoBackLeftAngle = autoBackLeftAngle - 5;}
+			if (autoBackLeftAngle < 0) {autoBackLeftAngle = 5 - autoBackLeftAngle;}
+			//									-1 to 1		   -2.5 to 2.5   0 to 5  whatever the fuck it wants to be
+			double autoFrontRightAngle	= (((Math.atan2(b, c) / Math.PI) * 2.5) + 2.5) + ANGLE_OFFSET3;
+		
+			if (autoFrontRightAngle > 5) {autoFrontRightAngle = autoFrontRightAngle - 5;}
+			if (autoFrontRightAngle < 0) {autoFrontRightAngle = 5 - autoFrontRightAngle;}
+
+			double autoFrontLeftAngle	= (((Math.atan2(b, d) / Math.PI) * 2.5) + 2.5) + ANGLE_OFFSET4;
+
+			if (autoFrontLeftAngle > 5) {autoFrontLeftAngle = autoFrontLeftAngle - 5;}
+			if (autoFrontLeftAngle < 0) {autoFrontLeftAngle = 5 - autoFrontLeftAngle;}
+
+			//normalize wheel speeds
+       	    double max = autoBackRightSpeed;
+
+        	if (autoBackLeftSpeed > max)   { max = autoBackLeftSpeed;}
+       		if (autoFrontLeftSpeed > max)  { max = autoFrontLeftSpeed;}
+        	if (autoFrontRightSpeed > max) { max = autoFrontRightSpeed;}
+
+        	if (max > 1) {
+
+            	autoBackRightSpeed  /= max;
+            	autoBackLeftSpeed   /= max;
+            	autoFrontLeftSpeed  /= max;
+            	autoFrontRightSpeed /= max;
+			}
+
+			backRight.drive(autoBackRightSpeed, autoBackRightAngle, false);
+			backLeft.drive(autoBackLeftSpeed, autoBackLeftAngle, false);
+			frontRight.drive(autoFrontRightSpeed, autoFrontRightAngle, false);
+			frontLeft.drive(autoFrontLeftSpeed, autoFrontLeftAngle, false);
+		}
 		
 		
 		public SwerveDrive (WheelDrive backRight, WheelDrive backLeft, WheelDrive frontRight, WheelDrive frontLeft ) {
